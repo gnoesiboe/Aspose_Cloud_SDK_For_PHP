311c381af3199a08b744da8e564c0df5185ee743
Assad Mahmood Qazi <assadvirgo@gmail.com>
2014-04-29 10:20:56 +0500

    Chart Legends using Aspose for Cloud 1.1.6.6

--- src/Aspose/Cloud/Cells/ChartEditor.php
+++ src/Aspose/Cloud/Cells/ChartEditor.php
@@ -76,6 +76,86 @@ class ChartEditor {
             return $v_output;
 	}
 
+    public function showChartLegend($chartIndex) {
+        //check whether file is set or not
+        if ($this->fileName == '')
+            throw new Exception('No file name specified');
+        //check whether workshett name is set or not
+        if ($this->worksheetName == '')
+            throw new Exception('Worksheet name not specified');
+        $strURI = Product::$baseProductUri . '/cells/' . $this->fileName . '/worksheets/' . $this->worksheetName . '/charts/' . $chartIndex . '/legend';
+        $signedURI = Utils::sign($strURI);
+        $responseStream = Utils::processCommand($signedURI, 'PUT', '', '');
+        $v_output = Utils::validateOutput($responseStream);
+        if ($v_output === '') {
+            //Save doc on server
+            $folder = new Folder();
+            $outputStream = $folder -> GetFile($this->fileName);
+            $outputPath = AsposeApp::$outPutLocation . $this->fileName;
+            Utils::saveFile($outputStream, $outputPath);
+            return $outputPath;
+        } else
+            return $v_output;
+    }
+
+    public function hideChartLegend($chartIndex) {
+        //check whether file is set or not
+        if ($this->fileName == '')
+            throw new Exception('No file name specified');
+        //check whether workshett name is set or not
+        if ($this->worksheetName == '')
+            throw new Exception('Worksheet name not specified');
+        $strURI = Product::$baseProductUri . '/cells/' . $this->fileName . '/worksheets/' . $this->worksheetName . '/charts/' . $chartIndex . '/legend';
+        $signedURI = Utils::sign($strURI);
+        $responseStream = Utils::processCommand($signedURI, 'DELETE', '', '');
+        $v_output = Utils::validateOutput($responseStream);
+        if ($v_output === '') {
+            //Save doc on server
+            $folder = new Folder();
+            $outputStream = $folder -> GetFile($this->fileName);
+            $outputPath = AsposeApp::$outPutLocation . $this->fileName;
+            Utils::saveFile($outputStream, $outputPath);
+            return $outputPath;
+        } else
+            return $v_output;
+    }
+
+    public function updateChartLegend($chartIndex,$properties='') {
+        //check whether file is set or not
+        if ($this->fileName == '')
+            throw new Exception('No file name specified');
+        //check whether workshett name is set or not
+        if ($this->worksheetName == '')
+            throw new Exception('Worksheet name not specified');
+        $strURI = Product::$baseProductUri . '/cells/' . $this->fileName . '/worksheets/' . $this->worksheetName . '/charts/' . $chartIndex . '/legend';
+        $signedURI = Utils::sign($strURI);
+        $responseStream = Utils::processCommand($signedURI, 'POST', 'json', $properties);
+        $v_output = Utils::validateOutput($responseStream);
+        if ($v_output === '') {
+            //Save doc on server
+            $folder = new Folder();
+            $outputStream = $folder -> GetFile($this->fileName);
+            $outputPath = AsposeApp::$outPutLocation . $this->fileName;
+            Utils::saveFile($outputStream, $outputPath);
+            return $outputPath;
+        } else
+            return $v_output;
+    }
+
+    public function readChartLegend($chartIndex) {
+        //check whether file is set or not
+        if ($this->fileName == '')
+            throw new Exception('No file name specified');
+        //check whether workshett name is set or not
+        if ($this->worksheetName == '')
+            throw new Exception('Worksheet name not specified');
+        $strURI = Product::$baseProductUri . '/cells/' . $this->fileName . '/worksheets/' . $this->worksheetName . '/charts/' . $chartIndex . '/legend';
+        $signedURI = Utils::sign($strURI);
+        $responseStream = Utils::processCommand($signedURI, 'GET', 'json', '');
+        $json = json_decode($responseStream);
+        return $json->Legend;
+    }
+
 	/*
 	 * Gets ChartArea of a chart
 	 * $chartIndex
